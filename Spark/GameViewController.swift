bd37ca4e6d45e6e58bb0d89484609069960d7845
Alex Roman <alroman@Alexs-Mac-mini.local>
2021-10-18 03:54:30 -0700

    Added and loaded world scene

--- Spark/GameViewController.swift
+++ Spark/GameViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 import SceneKit
 
 class GameViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: SCNView!
+    var scene: SCNScene!
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+     
         setupScene()
         // Do any additional setup after loading the view.
     }
@@ -29,6 +32,10 @@ class GameViewController: UIViewController {
     }
 
     func setupScene(){
+        sceneView.allowsCameraControl = true
+        guard let scene = SCNScene(named: "Art.scnassets/Earth copy.scn") else { fatalError("Unable to load scene file")}
+        sceneView.scene = scene
+        
         
     }
     /*
