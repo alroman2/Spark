02527c6409741b4c109fe5d12e8513c3a33474b9
Alex <alex37888@gmail.com>
2021-11-12 01:27:47 -0800

    Added doccumentation to methods

--- Spark/Classes/Models/WorldObject.swift
+++ Spark/Classes/Models/WorldObject.swift
@@ -9,7 +9,7 @@ import Foundation
 import SceneKit
 
 class WorldObject {
-    public var refNode:SCNReferenceNode!
+    var refNode:SCNReferenceNode!
     
     init(){
         
@@ -17,6 +17,12 @@ class WorldObject {
     
     // --MARK: Public Methods
     
+    
+    /// Loads the world object with a referenced file. The file must be compatible with SCNRefereceNode accepted types.
+    /// - Parameters:
+    ///   - fileName: The name of the file asset
+    ///   - fileType: A compatible file type postfix
+    ///   - dir: A directory path to the file from the project root
      func loadModel(fileName: String, fileType: String, dir: String) throws {
         
         if let filePath = Bundle.main.path(forResource: fileName, ofType: fileType, inDirectory: dir) {
@@ -24,6 +30,7 @@ class WorldObject {
             let url = URL(fileURLWithPath: filePath)
             
             refNode = SCNReferenceNode(url: url)
+            refNode.name = "\(fileName)_Ref_Node"
             refNode?.load()
         } else {
             //throw error
